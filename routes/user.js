f7d101598ca23df54d6246ef3d80f4e42d110907
Mohammed Amer <mrbingo5.98@gmail.com>
2020-06-29 14:15:23 +0200

    upeditpass

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const user = require('../models/user');
 const express = require('express'),
     router = express.Router(),
     passport = require('passport'),
+    bcrypt = require('bcrypt'),
     middelware = require('../middelware/authentication')
 
 
@@ -31,22 +32,56 @@ router.get('/forget', (req, res) => {
     res.render('forget')
 })
 router.post('/forget', middelware.forgetPassword(), (req, res) => {})
-router.post('/forget:token', middelware.passResetConfirmation(), (req, res) => {})
-router.get('/resetpassword/:id', async(req, res) => {
+router.post('/forget/:token', middelware.passResetConfirmation(), (req, res) => {})
+router.put('/resetpassword/:id', (req, res) => {
     const id = req.params.id
-    try {
-        hashedpass = await bcrypt.hash(req.body.password, 10)
-        res.status(201)
-    } catch {
-        res.status(500).send("something went wrong!!")
-    }
-    user.findByIdAndUpdate(id, { password: hashedpass }, (err, updated) => {
+    user.findById(id, async(err, found) => {
         if (err) {
             req.flash('error', 'Somthing went wrong. Please try again.')
-            res.redirect('back')
-        } else {
-            req.flash('success', 'Passwored has successfully been reset.')
             res.redirect('/')
+        } else {
+            try {
+                hashedpass = await bcrypt.hash(req.body.password, 10)
+                res.status(201)
+                found.password = hashedpass
+                found.save()
+                req.flash('success', 'Passwored has successfully been reset.')
+                res.redirect('/')
+            } catch {
+                req.flash('error', 'Somthing went wrong. Please try again.')
+                res.status(500).redirect('/')
+            }
+
+        }
+    })
+})
+router.get('/editpassword', middelware.authenticationMiddleware(), (req, res) => {
+    res.render('editPass', { id: req.user._id })
+})
+router.put('/updatepassword/:id', (req, res) => {
+    const id = req.params.id
+    user.findById(id, async(err, found) => {
+        if (err) {
+            req.flash('error', 'Somthing went wrong. Please try again.')
+            res.redirect('/updatepassword/' + found._id)
+        } else {
+            if (await bcrypt.compare(req.body.oldPassword, user.password)) {
+                try {
+                    hashedpass = await bcrypt.hash(req.body.newPassword, 10)
+                    res.status(201)
+                    found.password = hashedpass
+                    found.save()
+                    req.flash('success', 'Passwored has successfully been reset.')
+                    res.redirect('/')
+                } catch {
+                    req.flash('error', 'Somthing went wrong. Please try again.')
+                    res.status(500).redirect('/updatepassword/' + found._id)
+                }
+
+            } else {
+                req.flash('error', 'Incorrect old Password.')
+                res.redirect('/updatepassword/' + found._id)
+            }
         }
     })
 })
