49d2f2cafb3fedf8fb1aeb5e640c37f2271fcee9
mrbingo98 <56658257+mrbingo98@users.noreply.github.com>
2020-07-30 18:09:26 +0200

    upto6

--- public/frontjs/landinPage.js
+++ public/frontjs/landinPage.js
@@ -1,15 +1,15 @@
-var images = new Array('/s1.jpg', '/s2.jpg', '/s3.jpg', '/s4.jpg', '/s5.jpg');
-var nextimage = 0;
-doSlideshow();
+// var images = new Array('/s1.jpg', '/s2.jpg', '/s3.jpg', '/s4.jpg', '/s5.jpg');
+// var nextimage = 0;
+// doSlideshow();
 
-function doSlideshow() {
-    if (nextimage >= images.length) { nextimage = 0; }
-    $('body')
-        .css('background-image', 'url("' + images[nextimage++] + '")').css('background-size', 'cover')
-        .fadeIn(1000, function() {
-            setTimeout(doSlideshow, 4000);
-        });
-}
+// function doSlideshow() {
+//     if (nextimage >= images.length) { nextimage = 0; }
+//     $('body')
+//         .css('background-image', 'url("' + images[nextimage++] + '")').css('background-size', 'cover')
+//         .fadeIn(1000, function() {
+//             setTimeout(doSlideshow, 4000);
+//         });
+// }
 
 document.getElementById('mapTrigger').addEventListener('click', function() {
     document.getElementById('mapContainer').classList.toggle('disabeled')
